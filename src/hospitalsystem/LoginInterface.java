b856d43894d7ef59098a36fe2cb46f2294a18c30
gideonotieno <gideonotieno99@gmail.com>
2024-10-25 21:40:40 +0300

    added login fxml

---
+++ src/hospitalsystem/LoginInterface.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalsystem;
+
+/**
+ *
+ * @author Bruce
+ */
+public class LoginInterface extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setTitle("Hospital System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
